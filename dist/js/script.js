a5a4fc9dd79ef68b85fb70fc551322b8dbf3baf1
Shahadat Hossain <shahadat3669@gmail.com>
2022-11-15 01:46:35 +0600

    Close the mobile menu on menu item click

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,10 @@
 const navbar = document.querySelector(".header-navbar");
 
-
 const menuToggler = () => {
   const navbar = document.querySelector(".header-navbar");
   navbar.style.display = "flex";
-
-  
 };
 
-function menucloseToggler (){
-  navbar.style.display ="none";
+function menucloseToggler() {
+  navbar.style.display = "none";
 }
