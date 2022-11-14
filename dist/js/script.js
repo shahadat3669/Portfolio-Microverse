4f1cfbcb0e6d3252538ce8f3f36653df2bcbb92f
Glenda Diaz <glendadiazah@outlook.com>
2022-11-14 12:21:17 -0600

    Fix style for hamburger and closing button

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,13 @@
+const navbar = document.querySelector(".header-navbar");
+
+
 const menuToggler = () => {
   const navbar = document.querySelector(".header-navbar");
-  navbar.style.display = "block";
+  navbar.style.display = "flex";
+
+  
 };
+
+function menucloseToggler (){
+  navbar.style.display ="none";
+}
