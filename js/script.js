8e8b555d5ef5b19a0be3eba7e5a19aad99778c69
Glenda Diaz <glendadiazah@outlook.com>
2022-11-16 10:19:31 -0600

    Add js to open and close popup window

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const navbar = document.querySelector('.header-navbar');
-const headerHamburger = document.querySelector('.header-hamburger');
+const headerHamburger = document.querySelector('#header-hamburger');
 const headerClose = document.querySelector('.header-close');
+const popupwindow = document.querySelector('#popup');
+const closebt = document.querySelector('.popup-cancel');
+const seetp = document.querySelectorAll('.btn-project');
 
 function menuToggler() {
   const navbar = document.querySelector('.header-navbar');
@@ -11,5 +14,16 @@ function menucloseToggler() {
   navbar.style.display = 'none';
 }
 
+function popupWC() {
+  popupwindow.style.display = 'none';
+}
+function popupW() {
+  popupwindow.style.display = 'flex';
+}
+
 headerHamburger.addEventListener('click', menuToggler);
 headerClose.addEventListener('click', menucloseToggler);
+closebt.addEventListener('click', popupWC);
+seetp.forEach(box => {
+  box.addEventListener('click', popupW)
+})
